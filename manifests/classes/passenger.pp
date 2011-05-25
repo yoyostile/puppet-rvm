3776db10af42d2ba5f9b2bc6f226517de3947042
Omar Qureshi <omaraliqureshi@googlemail.com>
2011-05-25 23:33:54 +0100

    Move and tidy whitespace

--- manifests/classes/passenger.pp
+++ manifests/classes/passenger.pp
@@ -1,12 +1,12 @@
 class rvm::passenger::apache(
-    $ruby_version,
-    $version,
-    $rvm_prefix = '/usr/local/',
-    $mininstances = '1',
-    $maxpoolsize = '6',
-    $poolidletime = '300',
-    $maxinstancesperapp = '0',
-    $spawnmethod = 'smart-lv2'
+  $ruby_version,
+  $version,
+  $rvm_prefix = '/usr/local/',
+  $mininstances = '1',
+  $maxpoolsize = '6',
+  $poolidletime = '300',
+  $maxinstancesperapp = '0',
+  $spawnmethod = 'smart-lv2'
 ) {
 
     include rvm::passenger::passenger-gem
