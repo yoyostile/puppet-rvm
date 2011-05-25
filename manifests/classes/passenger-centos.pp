2b379d71d2087777d5b66e0de10c66cde241924e
Omar Qureshi <omaraliqureshi@googlemail.com>
2011-05-25 16:39:14 +0100

    Split up the passenger config so that it works with Ubuntu and CentOS

---
+++ manifests/classes/passenger-centos.pp
@@ -0,0 +1,23 @@
+class rvm::passenger::apache::ubuntu {
+
+  # Dependencies
+  if ! defined(Package["httpd"]) { package { "httpd": ensure => installed } }
+  if ! defined(Package["httpd-devel"]) { package { "httpd-devel":  ensure => installed } }
+  
+  exec {
+    'passenger-install-apache2-module':
+        command => "${binpath}rvm ${ruby_version} exec passenger-install-apache2-module -a",
+        creates => "${gempath}/passenger-${version}/ext/apache2/mod_passenger.so",
+        logoutput => 'on_failure',
+        require => [Rvm_gem['passenger'], Package['httpd','httpd-devel','mod_ssl']];
+  }
+  
+  file {
+    '/etc/httpd/passenger.load':
+      content => "LoadModule passenger_module ${gempath}/passenger-${version}/ext/apache2/mod_passenger.so",
+      ensure => file,
+      require => Exec['passenger-install-apache2-module'];
+  }
+  
+}
+  
--- manifests/classes/passenger.pp
+++ manifests/classes/passenger.pp
@@ -23,7 +23,6 @@ class rvm::passenger::apache(
 
     # TODO: How to inherit this from above?
     class { 'rvm::passenger': ruby_version => $ruby_version, version => $version, rvm_prefix => $rvm_prefix }
-    include apache
 
     # TODO: How can we get the gempath automatically using the ruby version
     # Can we read the output of a command into a variable?
@@ -32,91 +31,8 @@ class rvm::passenger::apache(
     $binpath = "${rvm_prefix}rvm/bin/"
 
 
-    # Dependencies
-    if ! defined(Package['build-essential'])      { package { build-essential:      ensure => installed } }
-    if ! defined(Package['apache2-prefork-dev'])  { package { apache2-prefork-dev:  ensure => installed } }
-    if ! defined(Package['libapr1-dev'])          { package { libapr1-dev:          ensure => installed, alias => 'libapr-dev' } }
-    if ! defined(Package['libaprutil1-dev'])      { package { libaprutil1-dev:      ensure => installed, alias => 'libaprutil-dev' } }
-    if ! defined(Package['libcurl4-openssl-dev']) { package { libcurl4-openssl-dev: ensure => installed } }
-
-    exec {
-        'passenger-install-apache2-module':
-            command => "${binpath}rvm ${ruby_version} exec passenger-install-apache2-module -a",
-            creates => "${gempath}/passenger-${version}/ext/apache2/mod_passenger.so",
-            logoutput => 'on_failure',
-            require => [Rvm_gem['passenger'], Package[
-                'apache2',
-                'build-essential',
-                'apache2-prefork-dev',
-                'libapr-dev',
-                'libaprutil-dev',
-                'libcurl4-openssl-dev'
-            ]];
-    }
-
-    file {
-        '/etc/apache2/mods-available/passenger.load':
-            content => "LoadModule passenger_module ${gempath}/passenger-${version}/ext/apache2/mod_passenger.so",
-            ensure => file,
-            require => Exec['passenger-install-apache2-module'];
-
-        '/etc/apache2/mods-available/passenger.conf':
-            content => template('rvm/passenger-apache.conf.erb'),
-            ensure => file,
-            require => Exec['passenger-install-apache2-module'];
-
-        '/etc/apache2/mods-enabled/passenger.load':
-            ensure => 'link',
-            target => '../mods-available/passenger.load',
-            require => File['/etc/apache2/mods-available/passenger.load'];
-
-        '/etc/apache2/mods-enabled/passenger.conf':
-            ensure => 'link',
-            target => '../mods-available/passenger.conf',
-            require => File['/etc/apache2/mods-available/passenger.conf'];
-    }
-
-    # Add Apache restart hooks
-    if defined(Service['apache']) {
-      File['/etc/apache2/mods-available/passenger.load'] ~> Service['apache']
-      File['/etc/apache2/mods-available/passenger.conf'] ~> Service['apache']
-      File['/etc/apache2/mods-enabled/passenger.load']   ~> Service['apache']
-      File['/etc/apache2/mods-enabled/passenger.conf']   ~> Service['apache']
-    }
-    if defined(Service['apache2']) {
-      File['/etc/apache2/mods-available/passenger.load'] ~> Service['apache2']
-      File['/etc/apache2/mods-available/passenger.conf'] ~> Service['apache2']
-      File['/etc/apache2/mods-enabled/passenger.load']   ~> Service['apache2']
-      File['/etc/apache2/mods-enabled/passenger.conf']   ~> Service['apache2']
-    }
-    if defined(Service['httpd']) {
-      File['/etc/apache2/mods-available/passenger.load'] ~> Service['httpd']
-      File['/etc/apache2/mods-available/passenger.conf'] ~> Service['httpd']
-      File['/etc/apache2/mods-enabled/passenger.load']   ~> Service['httpd']
-      File['/etc/apache2/mods-enabled/passenger.conf']   ~> Service['httpd']
-    }
-}
-
-class rvm::passenger::apache::disable {
-
-    file {
-        '/etc/apache2/mods-enabled/passenger.load':
-            ensure => 'absent';
-        '/etc/apache2/mods-enabled/passenger.conf':
-            ensure => 'absent';
-    }
-
-    # Add Apache restart hooks
-    if defined(Service['apache']) {
-      File['/etc/apache2/mods-enabled/passenger.load']   ~> Service['apache']
-      File['/etc/apache2/mods-enabled/passenger.conf']   ~> Service['apache']
-    }
-    if defined(Service['apache2']) {
-      File['/etc/apache2/mods-enabled/passenger.load']   ~> Service['apache2']
-      File['/etc/apache2/mods-enabled/passenger.conf']   ~> Service['apache2']
-    }
-    if defined(Service['httpd']) {
-      File['/etc/apache2/mods-enabled/passenger.load']   ~> Service['httpd']
-      File['/etc/apache2/mods-enabled/passenger.conf']   ~> Service['httpd']
+    case $operatingsystem {
+      Ubuntu: { include rvm::passenger::apache::ubuntu }
+      CentOS: { include rvm::passenger::apache::centos }
     }
 }
