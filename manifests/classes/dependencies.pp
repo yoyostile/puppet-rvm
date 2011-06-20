9a116fa0c5521309db90efa50156622cdfcb5ef7
Johannes Heck <johannes.heck@kayoom.com>
2011-06-20 16:53:17 +0200

    Added support for debian

--- manifests/classes/dependencies.pp
+++ manifests/classes/dependencies.pp
@@ -2,5 +2,6 @@ class rvm::dependencies {
   case $operatingsystem {
     Ubuntu: { include rvm::dependencies::ubuntu }
     CentOS: { include rvm::dependencies::centos }
+    Debian: { include rvm::dependencies::debian }
   }
 }
